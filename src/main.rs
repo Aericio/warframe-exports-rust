22fca7dcd5479771c1a161008ad6e18ca210cc84
aericio <16523741+Aericio@users.noreply.github.com>
2025-01-20 12:56:38 +0000

    feat: download export jsons

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::error::Error;
 use std::fs;
 use std::io::{BufReader, Cursor};
 use std::path::Path;
+use std::string::ToString;
 
 /// Environment Variables
 /// WARFRAME_CONTENT_URL
@@ -14,16 +15,22 @@ use std::path::Path;
 
 static STORAGE_FOLDERS: [&'static str; 3] = ["./output", "./output/image", "./output/export"];
 
-static EXPORT_HASH_LOCATION: &str = "./output/export_hash.json";
-static IMAGE_HASH_LOCATION: &str = "./output/image_hash.json";
+static EXPORT_HASH_LOCATION: &'static str = "./output/export_hash.json";
+static IMAGE_HASH_LOCATION: &'static str = "./output/image_hash.json";
 
-static LZMA_URL_PATH: &str = "/PublicExport/index_en.txt.lzma";
+static WARFRAME_CONTENT_URL: &'static str = "https://content.warframe.com";
+static LZMA_URL_PATH: &'static str = "/PublicExport/index_en.txt.lzma";
+static MANIFEST_PATH: &'static str = "/PublicExport/Manifest/";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let content_url = env::var("WARFRAME_CONTENT_URL").expect("Missing WARFRAME_CONTENT_URL");
+    // An HTTP client to share between all requests.
+    let client = Client::new();
+    
+    // A utility function for unwrapping results with no default value.
+    let unwrap_none = "None".to_string();
 
-    // Create missing data folders
+    // Create missing data folders.
     for folder in STORAGE_FOLDERS {
         if Path::new(folder).is_dir() == false {
             println!("{} directory not found, initializing...", folder);
@@ -31,10 +38,87 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let lzma_url = content_url.clone() + LZMA_URL_PATH;
+    let export_indices = fetch_export_indices(&client).await?;
+    // println!("{:#?}", export_indices);
+
+    let mut export_hashes: HashMap<String, String> = HashMap::new();
+    if Path::new(EXPORT_HASH_LOCATION).is_file() {
+        let existing_hashes = fs::read_to_string(EXPORT_HASH_LOCATION)?;
+        export_hashes = serde_json::from_str(&existing_hashes)?;
+        println!("{:#?}", export_hashes);
+    }
+
+    // let re = Regex::new(r"(_en|\.json)").unwrap();
+    let mut updated_hash = false;
+    let mut lines = export_indices.lines();
+    while let Some(line) = lines.next() {
+        // println!("{:#?}", line);
+        if let Some((resource_name, resource_hash)) = line.split_once("!") {
+            let existing_resource = export_hashes.get(resource_name).unwrap_or(&unwrap_none);
+
+            // Matching resource was found
+            if existing_resource == resource_hash {
+                continue;
+            }
+
+            updated_hash = true;
+            
+            // Got None, meaning a new resource.
+            if *existing_resource == unwrap_none {
+                println!(
+                    "Added a new resource ➞ {} ({})",
+                    resource_name, resource_hash
+                );
+            } else {
+                // An updated resource was found.
+                println!(
+                    "Updated an existing resource ➞ {} ({} from {})",
+                    resource_name, resource_hash, existing_resource
+                );
+            }
+
+            // download json files
+            let manifest_url = format!("{}{}{}", WARFRAME_CONTENT_URL, MANIFEST_PATH, line);
+            println!("Downloading... {}", &manifest_url);
+
+            let response = client
+                .get(Url::parse(&manifest_url)?)
+                .send()
+                .await?;
+
+            let text = response.text().await?;
+            let json = serde_json::to_string(&text)?;
+
+            println!("Wrote file... {}", resource_name);
+
+            fs::write(
+                format!("{}/{}", STORAGE_FOLDERS.get(2).unwrap(), resource_name),
+                json,
+            )?;
+
+            export_hashes.insert(resource_name.to_string(), resource_hash.to_string());
+        }
+    }
+
+    if updated_hash {
+        let json = serde_json::to_string(&export_hashes)?;
+        // println!("{:#?}", json);
+
+        println!("Wrote hashes to {}", EXPORT_HASH_LOCATION);
+        fs::write(EXPORT_HASH_LOCATION, json)?;
+    } else {
+        println!("No hashes to update!");
+    }
+
+    Ok(())
+}
+
+async fn fetch_export_indices(client: &Client) -> Result<String, Box<dyn Error>> {
+    let origin_url = env::var("WARFRAME_ORIGIN_URL").expect("Missing WARFRAME_ORIGIN_URL");
+    let lzma_url = origin_url.clone() + LZMA_URL_PATH;
     println!("{}", &lzma_url);
 
-    let response = Client::new()
+    let response = client
         .get(Url::parse(&lzma_url)?)
         .header(
             "Authentication",
@@ -51,51 +135,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
     lzma_rs::lzma_decompress(&mut reader, &mut decomp)?;
     let out = std::str::from_utf8(&decomp)?;
 
-    println!("{:#?}", out);
-
-    let mut export_hashes: HashMap<String, String> = HashMap::new();
-    if Path::new(EXPORT_HASH_LOCATION).is_file() {
-        let existing_hashes = fs::read_to_string(EXPORT_HASH_LOCATION)?;
-        export_hashes = serde_json::from_str(&existing_hashes)?;
-        println!("{:#?}", export_hashes);
-    }
-
-    let mut update_list: Vec<&str> = Vec::new();
-
-    // let re = Regex::new(r"(_en|\.json)").unwrap();
-    let mut lines = out.lines();
-    while let Some(line) = lines.next() {
-        // println!("{:#?}", line);
-        if let Some((resource_name, resource_hash)) = line.split_once("!") {
-            let existing_resource = export_hashes.get(resource_name);
-
-            if existing_resource.is_none() {
-                println!(
-                    "Added a new resource ➞ {} ({})",
-                    resource_name, resource_hash
-                );
-                update_list.push(line);
-            } else if existing_resource.unwrap() != resource_hash {
-                println!(
-                    "Updated an existing resource ➞ {} ({} from {})",
-                    resource_name,
-                    resource_hash,
-                    existing_resource.unwrap()
-                );
-                update_list.push(line);
-            }
-
-            export_hashes.insert(resource_name.to_string(), resource_hash.to_string());
-        }
-    }
-
-    let json = serde_json::to_string(&export_hashes)?;
-    println!("{:#?}", json);
-
-    println!("Wrote hashes to {}", EXPORT_HASH_LOCATION);
-    fs::write(EXPORT_HASH_LOCATION, json)?;
-
-    println!("Update list {}", update_list.join(" "));
-
-    Ok(())
+    Ok(out.to_string())
 }
